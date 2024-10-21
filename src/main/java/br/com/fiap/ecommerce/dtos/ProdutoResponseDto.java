0b9fc893e67025ec5b0594b776c41508a83001f6
thouser12 <thodavila@gmail.com>
2024-10-21 18:03:38 -0300

    first commit

---
+++ src/main/java/br/com/fiap/ecommerce/dtos/ProdutoResponseDto.java
@@ -0,0 +1,15 @@
+package br.com.fiap.ecommerce.dtos;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoResponseDto {
+    private Long id;
+	private String nome;
+	private BigDecimal valor;
+       
+}
